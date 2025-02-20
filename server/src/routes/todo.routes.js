d42031d81cf7d39132b0b277a1e6aab219b9c8c1
i-ankit-here <closetooankit@gmail.com>
2025-02-21 01:28:33 +0530

    routes for todo added

---
+++ server/src/routes/todo.routes.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { getTodos,addTodo,deleteTodo } from "../controllers/todo.controller.js";
+
+const router = Router();
+
+router.route("/getTodos").get(verifyJWT,getTodos);
+router.route("/addTodo").post(verifyJWT,addTodo);
+router.route("/deleteTodo/:id").get(verifyJWT,deleteTodo);
+
+export default router;
