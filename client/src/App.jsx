89361bc54d308e49662592a21b218bf2c1618d71
i-ankit-here <closetooankit@gmail.com>
2025-02-21 01:25:21 +0530

    added routes for register and login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,32 @@
 import './App.css'
-import ReactDOM from "react-dom/client";
+import {useEffect, useState} from "react"
+import ThemeContext from './themeContext';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from './components/Navbar'
 import MajorTodo from './components/majorTodo'
+import Register from './components/register';
+import Login from './components/login';
 
 function App() {
 
+  const _theme = localStorage.getItem('theme');
+  const [theme,setTheme] = useState(_theme?_theme:"light")
+  useEffect(()=>{
+    document.body.style.background = theme == "light"?"white":"#171616";
+  },[theme])
   return (
-    <>
+    <ThemeContext.Provider value={[theme,setTheme]}>
       <Navbar />
       <BrowserRouter>
         <Routes>
           <Route path='/'>
-          <Route index element = {<MajorTodo/>}/>
+          <Route path="dashboard" element = {<MajorTodo/>}/>
+          <Route path='register' element = {<Register/>}/>
+          <Route path='login' element = {<Login/>}/>
           </Route>
         </Routes>
       </BrowserRouter>
-    </>
+    </ThemeContext.Provider>
   )
 }
 
