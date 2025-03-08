8cf343f768f24a6f88815d65cdf22ed616aba692
i-ankit-here <closetooankit@gmail.com>
2025-03-08 23:19:29 +0530

    added environment

--- client/getEnvironment.js
+++ client/getEnvironment.js
@@ -1,4 +1,10 @@
 export default function(){
-    const arr = ["http://localhost:8010"]
+    const evr = window.origin;
+    const arr = ["http://localhost:8010","https://ihavetodo.vercel.app"]
+    const n = arr.length();
+    for(let i = 0;i<n;i++){
+        const item = arr[i];
+        if(item.includes(evr))return item;
+    }
     return arr[0];
 }
