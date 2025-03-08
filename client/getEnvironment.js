56d29b690d9e6ea016d4d2fb195ca1cda5b796a7
i-ankit-here <closetooankit@gmail.com>
2025-03-08 23:23:20 +0530

    bug fixed

--- client/getEnvironment.js
+++ client/getEnvironment.js
@@ -1,7 +1,7 @@
 export default function(){
     const evr = window.origin;
     const arr = ["http://localhost:8010","https://ihavetodo.vercel.app"]
-    const n = arr.length();
+    const n = arr.length;
     for(let i = 0;i<n;i++){
         const item = arr[i];
         if(item.includes(evr))return item;
