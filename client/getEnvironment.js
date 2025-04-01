8662bed84610464d1a4899d8e9838fb9fefbd0cb
i-ankit-here <closetooankit@gmail.com>
2025-04-02 00:06:03 +0530

    changed apiURL

--- client/getEnvironment.js
+++ client/getEnvironment.js
@@ -1,10 +1,10 @@
 export default function(){
     const evr = window.origin;
-    const arr = ["http://localhost:8010","https://ihavetodo.vercel.app"]
+    const arr = [[,"http://localhost:5173","http://localhost:8010"],["https://ihavtodo.netlify.app/","https://ihavetodo.onrender.com"]]
     const n = arr.length;
     for(let i = 0;i<n;i++){
-        const item = arr[i];
-        if(item.includes(evr))return item;
+        const item = arr[i][0];
+        if(item.includes(evr))return arr[i][1];
     }
-    return arr[0];
+    return arr[0][0];
 }
