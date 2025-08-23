0856dbc4f0aac641bc6ec1df6ed561ef17e920fe
ankit <closetooankit@gmail.com>
2025-08-23 23:53:16 +0530

    message model

---
+++ server/src/models/message.model.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema({
+  conversationId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Conversation'
+  },
+  sender: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  content: {
+    type: String,
+    required: true
+  }
+}, { timestamps: true }); 
+
+module.exports = mongoose.model('Message', messageSchema);
