276613301ca4a020aa51d5baf155a54c84b40a35
ankit <closetooankit@gmail.com>
2025-08-23 23:53:05 +0530

    conversation model

---
+++ server/src/models/conversation.model.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const conversationSchema = new mongoose.Schema({
+  participants: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  isGroupChat: {
+    type: Boolean,
+    default: false
+  },
+  groupName: {
+    type: String,
+    trim: true
+  },
+  groupAdmin: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Conversation', conversationSchema);
